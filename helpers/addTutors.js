bb509de8886e624c5bf89476dc6612a121de8099
root <root@uqueue.zones.eait.uq.edu.au>
2020-10-05 04:23:15 +0000

    Temp file for adding tutors to db

---
+++ helpers/addTutors.js
@@ -0,0 +1,18 @@
+var tutors = [
+	uqhkeigh,
+	uqbwebb2,
+	uqecavil,
+	uqpgrov1,
+	uqdjon14,
+	uqldeaki,
+	uqlkarst,
+	uqalim8,
+	uqaric13,
+	uqjmor24,
+	uqmpham6,
+	uqkbart5,
+].map(username => {user: username, role: 'tutor'});
+
+conn = new Mongo()
+db = conn.getDB('uqueue')
+db.courses.update({name: "CSSE1001"}, {$set: {staff: tutors}})
