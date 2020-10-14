1cbafa5946ef21d96f737f3029b9cf2f02ded722
root <root@uqueue.zones.eait.uq.edu.au>
2020-10-14 17:26:09 +1000

    Added jarnold to tutorlist

--- helpers/addTutors.js
+++ helpers/addTutors.js
@@ -1,20 +1,23 @@
 var tutors = [
-	uqhkeigh,
-	uqbwebb2,
-	uqecavil,
-	uqpgrov1,
-	uqdjon14,
-	uqldeaki,
-	uqlkarst,
-	uqalim8,
-	uqaric13,
-	uqjmor24,
-	uqmpham6,
-	uqkbart5,
+	"uqhkeigh",
+	"uqbwebb2",
+	"uqecavil",
+	"uqpgrov1",
+	"uqdjon14",
+	"uqldeaki",
+	"uqlkarst",
+	"uqalim8",
+	"uqaric13",
+	"uqjmor24",
+	"uqmpham6",
+	"uqkbart5",
+	"uqwkong",
+	"uqacoop8",
+	"uqjarno4",
 ].map(username => {
 	return { user: username, role: 'tutor'}
 });
 
 conn = new Mongo()
 db = conn.getDB('uqueue')
-db.courses.update({name: "CSSE1001"}, {$set: {staff: tutors}})
+db.courses.update({code: "CSSE1001"}, {$set: {staff: tutors}})
